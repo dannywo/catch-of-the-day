d1cb47cadb1e8f4c4fa88f0e3c87d764a6aca40d
Danny O <dannywo@gmail.com>
2021-01-23 15:05:46 -0800

    Adds Header, Inventory, and order components

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="top">
+                <h1>Catch
+                    <span className="ofThe">Of The</span>
+                    Day</h1>
+
+                <h3 className="tagline">
+                    <span>Fresh Seafood Market</span>
+                </h3>
+            </header>
+        )
+    }
+}
+
+export default Header;
