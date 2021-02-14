f1ea7561f67a4f0c0a505ced2574f9925538daf1
Danny O <dannywo@gmail.com>
2021-02-13 19:44:48 -0800

    Adds fish component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
 import SampleFishes from '../sample-fishes';
 
 
@@ -14,19 +15,16 @@ class App extends Component {
     addFish = fish => {
         // 1. take a copy of existing state
         const fishes = { ...this.state.fishes }
-        console.log(fishes);
         // 2. add our new fish to the fishes var
         fishes[`fish${Date.now()}`] = fish;
         // 4. Set the new fishes object to state
         this.setState({
             fishes: fishes
         })
-        console.log("Adding a fish")
     }
 
     loadSampleFishes = () => {
         this.setState({ fishes: SampleFishes })
-        alert("Loading Samples")
     }
 
     render() {
@@ -34,6 +32,16 @@ class App extends Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {
+                            Object.keys(this.state.fishes).map(key =>
+                                <Fish
+                                    key={key}
+                                    details={this.state.fishes[key]}
+                                />
+                            )
+                        }
+                    </ul>
                 </div>
                 <Order />
                 <Inventory
