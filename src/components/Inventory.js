2eb33e0f84fcdba7b4796c312fee87c0bca9a6fb
Danny O <dannywo@gmail.com>
2021-02-13 17:52:18 -0800

    Adds AddFishForm component

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import AddFirshForm from './AddFishForm';
 
 class Inventory extends Component {
     render() {
         return (
-            <div className="inventory">Inventory!</div>
+            <div className="inventory">
+                <h2>Inventory!</h2>
+                <AddFirshForm></AddFirshForm>
+            </div>
         )
     }
 }
