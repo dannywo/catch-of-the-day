bd6f2c9bf891daa6cc678ed0019fe1b1d775200f
Danny O <dannywo@gmail.com>
2023-04-22 14:57:32 -0700

    Missed commiting for updating fish change

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -8,7 +8,11 @@ class Inventory extends Component {
       <div className="inventory">
         <h2>Inventory!</h2>
         {Object.keys(this.props.fishes).map((key) => (
-          <EditFishForm fish={this.props.fishes[key]} />
+          <EditFishForm 
+            key={key}
+            index={key}
+            fish={this.props.fishes[key]} 
+            updateFish={this.props.updateFish}/>
         ))}
         <AddFishForm addFish={this.props.addFish}></AddFishForm>
         <button onClick={this.props.loadSampleFishes}>
